fbe652cf80fd0e96027f3f420d9175d350d17598
fisk810 <marc571m@edu.easj.dk>
2021-09-07 12:35:38 +0200

    added elapsed time

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,11 +7,19 @@ public class Main {
         double minute = 10.0;
         double second = 37.0;
 
+        int cHour = 14;
+        int cMinute = 20;
+        int cSecond = 54;
 
+
+        //number of seconds since midnight
         System.out.println(hour * 3600 + minute * 60 + second);
-
+        //number of seconds till midnight
         System.out.println(((24 - hour) * 3600) + ((59 - minute) * 60) + (60-second));
-
+        //percentage of day that has passed
         System.out.println("percentage of day that has passed: " + ((hour * 3600 + minute * 60 + second) / 86400 * 100) + "%");
+        //elapsed time since starting on the exercise in seconds
+        System.out.println(((cHour - hour) * 3600) + ((cMinute - minute) * 60) + (cSecond - second));
+
     }
 }
